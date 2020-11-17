4c80a54035cf6206d6c6361ce65ac19cbaefb1a3
nicop18 <nicolas.poitevin18@gmail.com>
2020-11-17 18:08:15 -0300

    Listado de products

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsList.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 let indexController ={
 
     index: function(req,res,next){
-        res.render('index');
+        res.render('index',{products});
     }
     
     
